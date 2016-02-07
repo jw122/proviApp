9f2b5921285d3fa8124b5e63a9f3f25a854d6ccb
Alice Sun <xian_sun@brown.edu>
2016-02-07 00:56:53 -0500

    got rid of shit

--- testApp/AlertViewController.swift
+++ testApp/AlertViewController.swift
@@ -7,18 +7,58 @@
 //
 
 import UIKit
+import AddressBook
 
 class AlertViewController: UIViewController {
     @IBOutlet weak var alertButton: UIButton!
+    let ACCOUNT_SID = "ACd7dc6d2d7b9518561275656fc46787e5"
+    let AUTH_TOKEN = "1ecb75e8fca8e5f65fa790c4e40602ff"
+    let TWILIO_NUM = "+19135433698"
+    let TO_NUMBER = "+14016123230"
+    let message = "HALP ME PLZ!!!!!"
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        //print("view loaded")
-        // Do any additional setup after loading the view.
+        
+//        let status = ABAddressBookGetAuthorizationStatus()
+//        if status == .Denied || status == .Restricted {
+//            // user previously denied, to tell them to fix that in settings
+//            return
+//        }
+//        
+//        // open it
+//        
+//        var error: Unmanaged<CFError>?
+//        let addressBook: ABAddressBook? = ABAddressBookCreateWithOptions(nil, &error)?.takeRetainedValue()
+//        if addressBook == nil {
+//            print(error?.takeRetainedValue())
+//            return
+//        }
+//        
+//        // request permission to use it
+//        
+//        ABAddressBookRequestAccessWithCompletion(addressBook) {
+//            granted, error in
+//            
+//            if !granted {
+//                // warn the user that because they just denied permission, this functionality won't work
+//                // also let them know that they have to fix this in settings
+//                return
+//            }
+//            
+//            if let people = ABAddressBookCopyArrayOfAllPeople(addressBook)?.takeRetainedValue() as? NSArray {
+//                // now do something with the array of people
+//                print(people)
+//            }
+//        }
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
     }
     
 
@@ -34,7 +74,22 @@ class AlertViewController: UIViewController {
     
     @IBAction func receivedClick(sender:UIButton!){
         print("hi the emergency button was clicked!!!")
+        let request = NSMutableURLRequest(URL: NSURL(string:"https://\(ACCOUNT_SID):\(AUTH_TOKEN)@api.twilio.com/2010-04-01/Accounts/\(ACCOUNT_SID)/SMS/Messages")!) //auth
+        request.HTTPMethod = "POST"
+        request.HTTPBody = "From=\(TWILIO_NUM)&To=\(TO_NUMBER)&Body=\(message)".dataUsingEncoding(NSUTF8StringEncoding) //parameters
         
+        // Build the completion block and send the request
+        NSURLSession.sharedSession().dataTaskWithRequest(request, completionHandler: { (data, response, error) in
+            print("Finished")
+            if let data = data, responseDetails = NSString(data: data, encoding: NSUTF8StringEncoding) {
+                // Success
+                print("Response: \(responseDetails)")
+            } else {
+                // Failure
+                print("Error: \(error)")
+            }
+        }).resume()
+        
+        print("hello it's me")
     }
-
 }
